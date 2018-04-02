7ba617c06ccebf6fd765e37d2a5d0b1dce77433a
jay-johnson <jay.p.h.johnson@gmail.com>
2018-04-02 12:29:03 -0700

    trying this

--- webapp/drf_network_pipeline/docs/source/conf.py
+++ webapp/drf_network_pipeline/docs/source/conf.py
@@ -27,7 +27,7 @@ if os.getenv("READTHEDOCS", "") != "":
     class Mock(MagicMock):
         @classmethod
         def __getattr__(cls, name):
-            return Mock()
+            return MagicMock()
 
     MOCK_MODULES = [
         'h5py',
