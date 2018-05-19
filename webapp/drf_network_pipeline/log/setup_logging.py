4e8d53b0189d18b793db65825b4ed3205d947d3a
jay-johnson <jay.p.h.johnson@gmail.com>
2018-05-19 13:05:01 -0700

    make sure the api supports the shared logger config like all other antinex components

--- webapp/drf_network_pipeline/log/setup_logging.py
+++ webapp/drf_network_pipeline/log/setup_logging.py
@@ -3,16 +3,23 @@ import json
 import logging.config
 
 
-def setup_logging(default_level=logging.INFO,
-                  default_path="{}/logging.json".format(
-                      os.getenv(
-                          "LOG_DIR",
-                          os.path.dirname(os.path.realpath(__file__)))),
-                  env_key="LOG_CFG",
-                  config_name=None):
+def setup_logging(
+        default_level=logging.INFO,
+        default_path="{}/logging.json".format(
+            os.getenv(
+                "LOG_DIR",
+                os.path.dirname(os.path.realpath(__file__)))),
+        env_key="LOG_CFG",
+        config_name=None):
+    """setup_logging
 
-    """
     Setup logging configuration
+
+    :param default_level: level to log
+    :param default_path: path to config (optional)
+    :param env_key: path to config in this env var
+    :param config_name: filename for config
+
     """
     path = default_path
     file_name = default_path.split("/")[-1]
@@ -33,7 +40,7 @@ def setup_logging(default_level=logging.INFO,
         return
 
     else:
-        cwd_path = os.getcwd() + "/network_pipeline/log/{}".format(
+        cwd_path = os.getcwd() + "/drf_network_pipeline/log/{}".format(
                                     file_name)
         if os.path.exists(cwd_path):
             with open(cwd_path, "rt") as f:
@@ -105,15 +112,56 @@ def build_colorized_logger(
     :param log_level: level to log
     :param log_config_path: path to log config file
     """
-    use_config = ("./log/{}").format(
-                    "{}".format(
-                        config))
-    if not os.path.exists(use_config):
-        use_config = log_config_path
-        if not os.path.exists(use_config):
-            use_config = ("./drf_network_pipeline/log/{}").format(
-                            "logging.json")
+
+    override_config = os.getenv(
+        "SHARED_LOG_CFG",
+        None)
+    debug_log_config = os.getenv(
+        "DEBUG_SHARED_LOG_CFG",
+        None)
+    if override_config:
+        if debug_log_config:
+            print((
+                "creating logger config env var: "
+                "SHARED_LOG_CFG={}".format(
+                    override_config)))
+        if os.path.exists(override_config):
+            setup_logging(
+                default_level=log_level,
+                default_path=override_config)
+            return logging.getLogger(name)
+        if debug_log_config:
+            print((
+                "Failed to find log config using env var: "
+                "SHARED_LOG_CFG={}".format(
+                    override_config)))
+    else:
+        if debug_log_config:
+            print((
+                "Not using shared logging env var: "
+                "SHARED_LOG_CFG={}".format(
+                    override_config)))
+    # allow a shared log config across all components
+
+    use_config = ("{}").format(
+        config)
+
     # find the log processing
+    if not os.path.exists(use_config):
+        use_config = ("./drf_network_pipeline/log/{}").format(
+                            config)
+        if not os.path.exists(use_config):
+            use_config = log_config_path
+            if not os.path.exists(use_config):
+                use_config = ("./log/{}").format(
+                            config)
+                if not os.path.exists(use_config):
+                    use_config = ("./drf_network_pipeline/log/{}").format(
+                                "logging.json")
+                # find the last log config backup from the base of the repo
+            # find the log config from the defaults with the env LOG_CFG
+        # find the log config from the base of the repo
+    # find the log config by the given path
 
     setup_logging(
         default_level=log_level,
