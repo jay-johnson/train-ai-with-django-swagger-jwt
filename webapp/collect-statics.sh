e9c358e151e21b65ae8a5b6aa51889447af6a35c
jay-johnson <jay.p.h.johnson@gmail.com>
2018-05-19 13:09:37 -0700

    allow collect statics to run using the correct venv in the container

--- webapp/collect-statics.sh
+++ webapp/collect-statics.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+venv=~/.venvs/venvdrfpipeline
+
+# support for using venv in other locations
+if [[ "${USE_VENV}" != "" ]]; then
+    if [[ -e ${USE_VENV}/bin/activate ]]; then
+        echo "Using custom virtualenv: ${USE_VENV}"
+        venv=${USE_VENV}
+    else
+        echo "Did not find custom virtualenv: ${USE_VENV}"
+        exit 1
+    fi
+fi
+
+echo "Activating and installing pips"
+. ${venv}/bin/activate
+echo ""
+
 if [[ "${COLLECT_STATICS}" == "1" ]]; then
-    echo ""
     echo "Collecting static files"
     python manage.py collectstatic --noinput >> /dev/null
     echo ""
