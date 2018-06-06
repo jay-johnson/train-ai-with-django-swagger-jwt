d932941cc2f23c7a034806682ab82aed2c3369fd
jay-johnson <jay.p.h.johnson@gmail.com>
2018-06-06 16:24:48 -0700

    user creation handled by the test tool now

--- openshift/create-user.sh
+++ openshift/create-user.sh
@@ -18,18 +18,11 @@ source ${user_file}
 # env | grep ANTINEX_ | sort
 # echo ""
 
-echo ""
 export ANTINEX_URL=$(./get-api-url.sh)
-echo "Creating user: ${ANTINEX_USER} on ${ANTINEX_URL}"
 ../tests/create-user.sh
 if [[ "${?}" != "0" ]]; then
     echo "Failed creating OpenShift user: ${ANTINEX_USER}"
     exit 1
 fi
-
-echo ""
-echo "Verifying user: ${ANTINEX_USER} on ${ANTINEX_URL}"
-./get-token.sh
-echo ""
 
 exit 0
